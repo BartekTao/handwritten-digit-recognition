dc27dc999952c6a2e97d19097abc4654dfdd589a
BartekTao <pony830906@gmail.com>
2022-10-29 09:40:41 +0800

    no.3

--- lab2_hw.py
+++ lab2_hw.py
@@ -15,7 +15,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
 
-        self.fc1 = nn.Linear(in_features=(64 * 128 * 128), out_features=128)
+        self.fc1 = nn.Linear(in_features=(64 * 16 * 16), out_features=128)
         self.fc2 = nn.Linear(in_features=128, out_features=10)
 
         # ==========================
@@ -82,8 +82,8 @@ def training(model, device, train_loader, criterion, optimizer):
         train_acc += calc_acc(output, target)
         train_loss += loss.item()
 
-    train_acc /= len(train_loader)
-    train_loss /= len(train_loader)
+    train_acc = train_acc/len(train_loader)
+    train_loss = train_loss/len(train_loader)
 
     return train_acc, train_loss
 
@@ -117,8 +117,8 @@ def validation(model, device, valid_loader, criterion):
 
             # ================================
 
-    valid_acc /= len(valid_loader)
-    valid_loss /= len(valid_loader)
+    valid_acc = valid_acc/len(valid_loader)
+    valid_loss = valid_loss.item()/len(valid_loader)
 
     return valid_acc, valid_loss
 
@@ -135,9 +135,8 @@ def main():
     # you can add your parameters here
     LEARNING_RATE = 0.01
     BATCH_SIZE = 50
-    EPOCHS = 2
-    TRAIN_DATA_PATH = './data/train'
-    # TRAIN_DATA_PATH = '/content/drive/MyDrive/data/train'
+    EPOCHS = 100
+    TRAIN_DATA_PATH = '/content/drive/MyDrive/data/train'
     VALID_DATA_PATH = '/content/drive/MyDrive/data/train'
     MODEL_PATH = 'hanwritten-digit.pt'
 
@@ -148,12 +147,14 @@ def main():
     # TODO 11: transforms
     train_transform = transforms.Compose([
         # may be adding some data augmentations?
-
+        transforms.Resize(36),
+        transforms.RandomResizedCrop(32, (0.5, 0.8)),
         transforms.ToTensor(),
         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
     ])
     # ===================
     valid_transform = transforms.Compose([
+        transforms.Resize(32),
         transforms.ToTensor(),
         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
     ])
@@ -163,10 +164,15 @@ def main():
     # TODO 12: set up datasets
     # hint: ImageFolder?
     train_data = datasets.ImageFolder(TRAIN_DATA_PATH, transform=train_transform)
+    valid_data = datasets.ImageFolder(TRAIN_DATA_PATH, transform=valid_transform)
+
     #切分80%當作訓練集、20%當作驗證集
-    train_size = int(0.8 * len(train_data))
-    valid_size = len(train_data) - train_size
-    train_data, valid_data = torch.utils.data.random_split(train_data, [train_size, valid_size])
+    train_size = int(0.8 * len(valid_data))
+    valid_size = len(valid_data) - train_size
+    # train_data, _ = torch.utils.data.random_split(train_data, [train_size, valid_size])
+    train_data2, valid_data = torch.utils.data.random_split(valid_data, [train_size, valid_size])
+
+    train_data = torch.utils.data.ConcatDataset([train_data2, train_data])
     
     # train_data = datasets.ImageFolder(TRAIN_DATA_PATH, transform=train_transform)
     # valid_data = datasets.ImageFolder(VALID_DATA_PATH, transform=valid_transform)
@@ -176,7 +182,7 @@ def main():
 
     # ============================
     # TODO 13 : set up dataloaders
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_data2, batch_size=BATCH_SIZE, shuffle=True)
     valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=BATCH_SIZE, shuffle=True)
     # ============================
 
@@ -216,15 +222,16 @@ def main():
     # TODO 16: draw accuracy and loss pictures
     # lab2_teamXX_accuracy.png, lab2_teamXX_loss.png
     # hint: plt.plot
+    plt.figure("loss")
     plt.plot(train_loss, label='Training loss')
     plt.plot(valid_loss, label='Valid loss')
     plt.legend()
-    
+
+    plt.figure("acc")
     plt.plot(train_acc, label='Training acc')
     plt.plot(valid_acc, label='Valid acc')
     plt.legend()
-
-    plt.grid()
+    plt.show()
 
 
     # =========================================
